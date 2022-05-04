009c16a15462e7dd302164d4cd99b779ece30586
Sazzad ur Rahman <22joylobo@gmail.com>
2022-05-04 17:45:02 +0600

    heroku setup

--- __setup_heroku.js
+++ __setup_heroku.js
@@ -4,4 +4,8 @@
  * 2. verify email
  * 3.install heroky cli from the website
  * 4.run heroku login
+ * ---------------
+ * 1.check if the server is running locally
+ * 2.run heroku creates
+ *
  */
